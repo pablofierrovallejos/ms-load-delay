333b1772384d7c7c1301a421e83038ac19babc5b
Pablo Fierro Vallejos <pablofierrovallejos@hotmail.com>
2025-01-24 16:56:28 -0800

    init commit

---
+++ src/main/java/com/pablo/dormir/dormir/DormirApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.dormir.dormir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/dormir/dormir/DormirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.dormir.dormir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
