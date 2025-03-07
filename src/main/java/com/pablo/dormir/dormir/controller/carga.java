10097f19665b5ab4b44d2e2d71398db58d4c5d0a
pablofierrovallejos <pablofierrovallejos@gmail.com>
2025-03-07 13:15:19 -0300

    add ep parametrizado para tiempo retardo millisegundos

--- src/main/java/com/pablo/dormir/dormir/controller/carga.java
+++ src/main/java/com/pablo/dormir/dormir/controller/carga.java
@@ -1,22 +1,35 @@
 package com.pablo.dormir.dormir.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class carga {
     @GetMapping("/carga")
     public void endpoint(){
-
         System.out.println("Iniciando Carga");
-
         try {
             Thread.sleep( 5000);
         } catch (InterruptedException ie) {
             Thread.currentThread().interrupt();
         }
-
         System.out.println("Cargado");
+    }
+
+
+
+    @GetMapping("/carga/{sRetardo}")
+    public String endpoint2(@PathVariable("sRetardo") String sRetardo){
+
+        int iretardo = Integer.parseInt(sRetardo);
+        System.out.println("Iniciando Carga");
+        try {
+            Thread.sleep( iretardo);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Cargado");
+        return "Retardo ejecutado por : " + iretardo + " ms";
     }
 }
